f7528a8cc79d4695ade3b1f89927973582ab0a43
Simon Krüger <70892166+siKruger@users.noreply.github.com>
2020-11-12 17:44:10 +0100

    initial commit

---
+++ src/main/java/de/simonkrueger/wakeuptimes/WakeuptimesApplication.java
@@ -0,0 +1,13 @@
+package de.simonkrueger.wakeuptimes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WakeuptimesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WakeuptimesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/simonkrueger/wakeuptimes/WakeuptimesApplicationTests.java
@@ -0,0 +1,13 @@
+package de.simonkrueger.wakeuptimes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WakeuptimesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
